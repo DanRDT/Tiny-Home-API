83896eb38dbf40974116ab40143abf21a607d678
Dan <pulberdan@gmail.com>
2024-10-08 20:28:45 -0400

    Create tasks put route

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import express, { Express, Request, Response } from 'express'
 import dotenv from 'dotenv'
-import { new_task_schema } from './types/zod'
+import { id_schema, new_task_schema, task_schema } from './types/zod'
 import { TasksTable } from './db/schema'
 import { db } from './db/db'
 import { fromZodError } from 'zod-validation-error'
+import { eq } from 'drizzle-orm'
 
 dotenv.config()
 
@@ -31,8 +32,29 @@ app.post('/tasks', async (req: Request, res: Response) => {
   }
 })
 
-app.put('/tasks/:id', (req: Request, res: Response) => {
-  res.status(200).send('')
+app.put('/tasks/:id', async (req: Request, res: Response) => {
+  const idObject = id_schema.safeParse(req.params.id)
+  const updatedTaskObject = task_schema.safeParse(req.body)
+
+  if (idObject.success !== true) {
+    res.status(400).send({ message: fromZodError(idObject.error).message })
+  } else if (updatedTaskObject.success !== true) {
+    res.status(400).send({ message: fromZodError(updatedTaskObject.error).message })
+  } else if (idObject.data !== updatedTaskObject.data.id) {
+    res.status(400).send({ message: "Id's don't match." })
+  } else {
+    const { data: updatedTask } = updatedTaskObject
+    const [createdTask] = await db
+      .update(TasksTable)
+      .set({ taskDescription: updatedTask.taskDescription, dueDate: updatedTask.dueDate, completed: updatedTask.completed })
+      .where(eq(TasksTable.id, updatedTask.id))
+      .returning()
+    if (createdTask) {
+      res.status(201).send(createdTask)
+    } else {
+      res.status(400).send({ message: 'Task Not Found' })
+    }
+  }
 })
 
 app.delete('/tasks/:id', (req: Request, res: Response) => {
--- src/types/zod.ts
+++ src/types/zod.ts
@@ -1,7 +1,14 @@
 import z from 'zod'
 
+export const id_schema = z.string().uuid()
+
 export const new_task_schema = z.object({
   taskDescription: z.string(),
   dueDate: z.string().datetime(),
   completed: z.boolean(),
+})
+
+export const task_schema = new_task_schema.extend({
+  id: id_schema,
+  createdDate: z.string().datetime(),
 })
